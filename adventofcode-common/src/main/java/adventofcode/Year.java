1e7f8b271cfccc046b1e54f90f1b5213c7538ab6
adventcoder <59347361+adventcoder@users.noreply.github.com>
2025-11-02 19:15:20 +0000

    add lifecycle info to maven plugin

--- adventofcode-common/src/main/java/adventofcode/Year.java
+++ adventofcode-common/src/main/java/adventofcode/Year.java
@@ -7,24 +7,29 @@ import org.reflections.Reflections;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 @Command(mixinStandardHelpOptions = true, description = "Run Advent of Code puzzles for a year")
 public class Year implements Callable<Integer> {
     public static void main(String[] args) throws Exception {
         CommandLine cmd = new CommandLine(new Year());
-        addDayCommands(cmd);
+        for (AbstractDay day : getDays())
+            cmd.addSubcommand(day.getClass().getSimpleName(), day);
         System.exit(cmd.execute(args));
     }
 
-    private static void addDayCommands(CommandLine cmd) throws Exception {
+    private static List<? extends AbstractDay> getDays() throws Exception {
         Reflections reflections = new Reflections("adventofcode");
-        List<Class<? extends AbstractDay>> dayClasses = new ArrayList<>(reflections.getSubTypesOf(AbstractDay.class));
-        dayClasses.sort(Comparator.comparing(Class::getSimpleName));
-        for (Class<?> dayClass : dayClasses) {
-            AbstractDay day = dayClass.asSubclass(AbstractDay.class).getConstructor().newInstance();
-            cmd.addSubcommand(dayClass.getSimpleName(), day);
-        }
+        Set<Class<? extends AbstractDay>> dayClasses = reflections.getSubTypesOf(AbstractDay.class);
+
+        List<AbstractDay> days = new ArrayList<>();
+        for (Class<? extends AbstractDay> dayClass : dayClasses)
+            days.add(dayClass.getConstructor().newInstance());
+
+        days.sort(Comparator.comparing(day -> day.day));
+
+        return days;
     }
 
     @Override
