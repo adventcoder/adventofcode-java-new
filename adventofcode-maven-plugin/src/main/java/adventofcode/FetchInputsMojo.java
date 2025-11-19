8ed190df2efeea4aca45865a034db5608aa4eb99
adventcoder <59347361+adventcoder@users.noreply.github.com>
2025-11-19 09:33:48 +0000

    simplify plugin

--- adventofcode-maven-plugin/src/main/java/adventofcode/FetchInputsMojo.java
+++ adventofcode-maven-plugin/src/main/java/adventofcode/FetchInputsMojo.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -13,6 +15,12 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.codehaus.plexus.util.FileUtils;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ParserConfiguration.LanguageLevel;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.AnnotationExpr;
 
 @Mojo(name = "fetch-inputs", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class FetchInputsMojo extends AbstractMojo {
@@ -35,41 +43,67 @@ public class FetchInputsMojo extends AbstractMojo {
     @Parameter(defaultValue = "adventofcode/year%d/Day%d.txt", required = true, readonly = true)
     private String inputPathFormat;
 
+    public File getInputFile(int day) {
+        return new File(resourceDirectory, String.format(inputPathFormat, year, day));
+    }
+
     @Override
     public void execute() throws MojoExecutionException {
         try {
-            SourceFileParser sourceFileParser = new SourceFileParser();
+            Set<Integer> days = getInputsToDownload();
+            if (days.isEmpty()) {
+                getLog().info("No inputs to download");
+                return;
+            }
 
-            List<File> sourceFiles = FileUtils.getFiles(sourceDirectory, sourceFilePattern, null);
-            for (File sourceFile : sourceFiles) {
+            getLog().info("Fetching " + days.size() + " inputs");
+            Client client = new Client(session);
+            for (int day : days) {
+                Path inputPath = getInputFile(day).toPath();
+                getLog().debug("Fetching input for day " + day + " -> " + inputPath);
 
-                getLog().info("Parsing source file " + sourceFile);
-                ParsedSourceFile parsedSourceFile = sourceFileParser.parse(sourceFile);
-
-                PuzzleInfo puzzleInfo = parsedSourceFile.getPuzzleInfo();
-                if (puzzleInfo != null)
-                    fetchInput(puzzleInfo.getDay());
+                Files.createDirectories(inputPath.getParent());
+                try (OutputStream out = Files.newOutputStream(inputPath)) {
+                    client.downloadInput(year, day, out);
+                } catch (IOException ioe) {
+                    Files.deleteIfExists(inputPath);
+                    throw ioe;
+                }
             }
         } catch (Exception e) {
             throw new MojoExecutionException("Failed fetching inputs", e);
         }
     }
 
-    private void fetchInput(int day) throws IOException {
-        Path inputPath = resourceDirectory.toPath().resolve(String.format(inputPathFormat, year, day));
-        if (Files.exists(inputPath)) {
-            getLog().info("Input already exists: " + inputPath);
-            return;
-        }
+    private Set<Integer> getInputsToDownload() throws Exception {
+        Set<Integer> days = getRequiredInputs();
+        days.removeIf(day -> getInputFile(day).exists());
+        return days;
+    }
 
-        getLog().info("Fetching input for day " + day + " -> " + inputPath);
+    private Set<Integer> getRequiredInputs() throws IOException {
+        JavaParser parser = new JavaParser();
+        parser.getParserConfiguration().setLanguageLevel(LanguageLevel.JAVA_17); //TODO: get this from standard maven property?
 
-        Files.createDirectories(inputPath.getParent());
-        try (OutputStream out = Files.newOutputStream(inputPath)) {
-            new Client(session).downloadInput(year, day, out);
-        } catch (IOException ioe) {
-            Files.deleteIfExists(inputPath);
-            throw ioe;
+        List<File> sourceFiles = FileUtils.getFiles(sourceDirectory, sourceFilePattern, null);
+        getLog().info("Scanning " + sourceFiles.size() + " source files");
+
+        Set<Integer> days = new HashSet<>();
+        for (File sourceFile : sourceFiles) {
+            getLog().debug("Parsing source file: " + sourceFile);
+            ParseResult<CompilationUnit> result = parser.parse(sourceFile);
+            if (!result.isSuccessful()) {
+                getLog().warn(sourceFile + ": " + result);
+                continue;
+            }
+
+            CompilationUnit cu = result.getResult().orElseThrow();
+            for (var type : cu.getTypes()) {
+                AnnotationExpr puzzleAnno = AstUtils.getPuzzleAnnotation(type, cu);
+                if (puzzleAnno != null)
+                    days.add(AstUtils.getPuzzleDay(puzzleAnno));
+            }
         }
+        return days;
     }
 }
