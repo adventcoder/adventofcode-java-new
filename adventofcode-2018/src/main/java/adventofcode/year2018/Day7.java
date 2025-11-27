b343d773e433f571c99f07c8a77fd4e96524fdd0
adventcoder <59347361+adventcoder@users.noreply.github.com>
2025-11-27 12:34:10 +0000

    unified scheduler meh

--- adventofcode-2018/src/main/java/adventofcode/year2018/Day7.java
+++ adventofcode-2018/src/main/java/adventofcode/year2018/Day7.java
@@ -3,6 +3,7 @@ package adventofcode.year2018;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -14,19 +15,12 @@ import adventofcode.utils.Fn;
 import adventofcode.utils.collect.Counter;
 import adventofcode.utils.collect.DefaultHashMap;
 import lombok.AllArgsConstructor;
-import picocli.CommandLine.Option;
 
 @Puzzle(day = 7, name = "The Sum of Its Parts")
 public class Day7 extends AbstractDay {
     public static void main(String[] args) throws Exception {
         main(Day7.class, args);
     }
-
-    @Option(names = "--workers", defaultValue = "5")
-    private int workers;
-
-    @Option(names = "--base-duration", defaultValue = "60")
-    private int baseDuration;
 
     private Map<String, Set<String>> edges;
 
@@ -41,42 +35,67 @@ public class Day7 extends AbstractDay {
 
     @Override
     public String part1() {
-        StepQueue queue = new StepQueue();
-        List<String> seq = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            String step = queue.poll();
-            seq.add(step);
-            queue.finished(step);
-        }
-        return String.join("", seq);
+        Scheduler scheduler = new Scheduler(1);
+        scheduler.run();
+        return String.join("", scheduler.finished);
     }
 
     @Override
     public Integer part2() {
-        int totalTime = 0;
-        StepQueue queue = new StepQueue();
-        List<Task> tasks = new ArrayList<>();
-        addTasks(queue, tasks);
-        while (!tasks.isEmpty()) {
-            int timeStep = Fn.min(tasks, task -> task.timeRemaining);
-            totalTime += timeStep;
-            tasks.removeIf(task -> {
-                task.timeRemaining -= timeStep;
-                if (task.timeRemaining == 0) {
-                    queue.finished(task.step);
-                    return true;
-                }
-                return false;
-            });
-            addTasks(queue, tasks);
-        }
-        return totalTime;
+        Scheduler scheduler = new Scheduler(5);
+        scheduler.run();
+        return scheduler.totalTime;
     }
 
-    private void addTasks(StepQueue queue, List<Task> tasks) {
-        while (tasks.size() < workers && !queue.isEmpty()) {
-            String step = queue.poll();
-            tasks.add(new Task(step, baseDuration + step.charAt(0) - 'A' + 1));
+    private class Scheduler {
+        private final int workers;
+        private final Counter<String> inDegree = new Counter<>();
+        private final PriorityQueue<String> available = new PriorityQueue<>();
+        private final List<Task> inProgress = new ArrayList<>();
+        private final List<String> finished = new ArrayList<>();
+        private int totalTime = 0;
+
+        public Scheduler(int workers) {
+            this.workers = workers;
+
+            for (String a : edges.keySet())
+                for (String b : edges.get(a))
+                    inDegree.inc(b);
+
+            for (String a : edges.keySet())
+                if (!inDegree.containsKey(a))
+                    available.add(a);
+        }
+
+        public void run() {
+            addTasks();
+            while (!inProgress.isEmpty()) {
+                int timeStep = Fn.min(inProgress, t -> t.timeRemaining);
+                totalTime += timeStep;
+                for (Iterator<Task> it = inProgress.iterator(); it.hasNext(); ) {
+                    Task task = it.next();
+                    task.timeRemaining -= timeStep;
+                    if (task.timeRemaining == 0) {
+                        finish(task.step);
+                        it.remove();
+                    }
+                }
+                addTasks();
+            }
+        }
+
+        public void addTasks() {
+            while (inProgress.size() < workers && !available.isEmpty()) {
+                String step = available.poll();
+                inProgress.add(new Task(step, 60 + (step.charAt(0) - 'A' + 1)));
+            }
+        }
+
+        public void finish(String step) {
+            for (String b : edges.getOrDefault(step, Collections.emptySet()))
+                if (inDegree.dec(b) == 0)
+                    available.add(b);
+            finished.add(step);
         }
     }
 
@@ -84,26 +103,5 @@ public class Day7 extends AbstractDay {
     private static class Task {
         public final String step;
         public int timeRemaining;
-    }
-
-    //TODO: this is kind of ugly
-    private class StepQueue extends PriorityQueue<String> {
-        private final Counter<String> inDegree = new Counter<>();
-
-        public StepQueue() {
-            for (String a : edges.keySet())
-                for (String b : edges.get(a))
-                    inDegree.inc(b);
-
-            for (String a : edges.keySet())
-                if (!inDegree.containsKey(a))
-                    add(a);
-        }
-
-        public void finished(String a) {
-            for (String b : edges.getOrDefault(a, Collections.emptySet()))
-                if (inDegree.dec(b) == 0)
-                    add(b);
-        }
     }
 }
