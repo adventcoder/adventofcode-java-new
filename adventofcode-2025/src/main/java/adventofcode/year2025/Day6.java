4a51c92e253e6725ff41af97eb5d1d9a9e283e31
adventcoder <59347361+adventcoder@users.noreply.github.com>
2025-12-06 21:14:07 +0000

    no grid

--- adventofcode-2025/src/main/java/adventofcode/year2025/Day6.java
+++ adventofcode-2025/src/main/java/adventofcode/year2025/Day6.java
@@ -2,14 +2,14 @@ package adventofcode.year2025;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.IntStream;
+import java.util.Map;
+import java.util.function.ToLongFunction;
 import java.util.stream.LongStream;
 
 import adventofcode.AbstractDay;
 import adventofcode.Puzzle;
-import adventofcode.utils.Fn;
-import adventofcode.utils.geom.Grid;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import it.unimi.dsi.fastutil.longs.LongList;
 
 @Puzzle(day = 6, name = "Trash Compactor")
 public class Day6 extends AbstractDay {
@@ -17,48 +17,74 @@ public class Day6 extends AbstractDay {
         main(Day6.class, args);
     }
 
-    private List<Grid> problems = new ArrayList<>();
+    private static Map<String, ToLongFunction<long[]>> opMap = Map.of(
+        "+", operands -> LongStream.of(operands).sum(),
+        "*", operands -> LongStream.of(operands).reduce(1L, (x, y) -> x * y)
+    );
+
+    private List<ToLongFunction<long[]>> operators;
+    private List<long[]> rowOperands;
+    private List<long[]> colOperands;
 
     @Override
     public void parse(String input) {
-        Grid sheet = Grid.parse(input);
-        int start = 0;
-        for (int x = 0; x < sheet.width; x++) {
-            if (sheet.getColumn(x).isBlank()) {
-                problems.add(sheet.slice(start, 0, x - start, sheet.height));
-                start = x + 1;
+        String[] rows = input.split("\n");
+        parseRows(rows);
+        parseColumns(rows);
+    }
+
+    private void parseRows(String[] rows) {
+        operators = new ArrayList<>();
+        rowOperands = new ArrayList<>();
+        for (String token : rows[rows.length - 1].trim().split("\\s+")) {
+            operators.add(opMap.get(token));
+            rowOperands.add(new long[rows.length - 1]);
+        }
+        for (int y = 0; y < rows.length - 1; y++) {
+            String[] tokens = rows[y].trim().split("\\s+");
+            for (int i = 0; i < tokens.length; i++)
+                rowOperands.get(i)[y] = Integer.parseInt(tokens[i]);
+        }
+    }
+
+    private void parseColumns(String[] rows) {
+        int maxLength = 0;
+        for (String row : rows)
+            maxLength = Math.max(maxLength, row.length());
+        colOperands = new ArrayList<>();
+        LongList curr = new LongArrayList();
+        for (int x = 0; x < maxLength; x++) {
+            String token = getColumn(rows, x).trim();
+            if (token.isEmpty()) {
+                colOperands.add(curr.toLongArray());
+                curr.clear();
+            } else {
+                curr.add(Long.parseLong(token));
             }
         }
-        problems.add(sheet.slice(start, 0, sheet.width - start, sheet.height));
+        colOperands.add(curr.toLongArray());
+    }
+
+    private String getColumn(String[] rows, int x) {
+        char[] arr = new char[rows.length - 1];
+        for (int y = 0; y < rows.length - 1; y++)
+            arr[y] = x < rows[y].length() ? rows[y].charAt(x) : ' ';
+        return new String(arr);
     }
 
     @Override
     public Long part1() {
-        return Fn.sumLong(problems, p -> eval(p, this::operandsByRow));
+        long total = 0;
+        for (int i = 0; i < operators.size(); i++)
+            total += operators.get(i).applyAsLong(rowOperands.get(i));
+        return total;
     }
 
     @Override
     public Long part2() {
-        return Fn.sumLong(problems, p -> eval(p, this::operandsByColumn));
+        long total = 0;
+        for (int i = 0; i < operators.size(); i++)
+            total += operators.get(i).applyAsLong(colOperands.get(i));
+        return total;
     }
-
-    private long eval(Grid problem, Function<Grid, LongStream> operands) {
-        String op = problem.getRow(problem.height - 1).trim();
-        return switch (op) {
-            case "+" -> operands.apply(problem).sum();
-            case "*" -> operands.apply(problem).reduce(1L, (x, y) -> x * y);
-            default -> throw new UnsupportedOperationException();
-        };
-    }
-
-    private LongStream operandsByRow(Grid problem) {
-        return IntStream.range(0, problem.height - 1)
-            .mapToLong(y -> Long.parseLong(problem.getRow(0, y, problem.width).trim()));
-    }
-
-    private LongStream operandsByColumn(Grid problem) {
-        return IntStream.range(0, problem.width)
-            .mapToLong(x -> Long.parseLong(problem.getColumn(x, 0, problem.height - 1).trim()));
-    }
-
 }
