5c46cbaa0bfe9bfb5086d2143f74fd53d06b9d9c
adventcoder <59347361+adventcoder@users.noreply.github.com>
2025-12-11 06:22:22 +0000

    ready

---
+++ adventofcode-2025/src/main/java/adventofcode/year2025/Day12.java
@@ -0,0 +1,20 @@
+package adventofcode.year2025;
+
+import adventofcode.AbstractDay;
+import adventofcode.Puzzle;
+
+@Puzzle(day = 12)
+public class Day12 extends AbstractDay {
+    public static void main(String[] args) throws Exception {
+        main(Day12.class, args);
+    }
+
+    @Override
+    public void parse(String input) {
+    }
+
+    @Override
+    public Object part1() {
+        return null;
+    }
+}
