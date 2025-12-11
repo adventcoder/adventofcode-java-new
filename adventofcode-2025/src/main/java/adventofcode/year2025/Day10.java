f2073504ea0516a1e3de3adc287ae505a6bc4d79
adventcoder <59347361+adventcoder@users.noreply.github.com>
2025-12-11 04:00:10 +0000

    wip

--- adventofcode-2025/src/main/java/adventofcode/year2025/Day10.java
+++ adventofcode-2025/src/main/java/adventofcode/year2025/Day10.java
@@ -2,11 +2,13 @@ package adventofcode.year2025;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.DoubleStream;
 
 import adventofcode.AbstractDay;
 import adventofcode.Puzzle;
 import adventofcode.utils.Fn;
 import adventofcode.utils.array.IntArrays;
+import it.unimi.dsi.fastutil.Pair;
 import it.unimi.dsi.fastutil.ints.IntObjectPair;
 
 @Puzzle(day = 10, name = "Factory")
@@ -15,12 +17,12 @@ public class Day10 extends AbstractDay {
         main(Day10.class, args);
     }
 
-    // @Override
-    // protected String getInput() throws IOException {
-    //     return "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n" +
-    //         "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n" +
-    //         "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}";
-    // }
+    @Override
+    protected String getInput() {
+        return "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n" +
+            "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n" +
+            "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}";
+    }
 
     private List<Machine> machines;
 
@@ -34,10 +36,10 @@ public class Day10 extends AbstractDay {
         int total = 0;
         for (Machine m : machines) {
             int leastButtons = Integer.MAX_VALUE;
-            IntObjectPair<int[]> sol = solveGF2(m.buttons, m.diagram);
+            var sol = solveGF2(m.buttons, m.diagram);
             int n = 1 << sol.right().length;
             for (int x = 0; x < n; x++) {
-                int mask = sol.leftInt() ^ multiplyGF2(sol.right(), x);
+                int mask = getSol(sol, x);
                 leastButtons = Math.min(leastButtons, Integer.bitCount(mask));
             }
             total += leastButtons;
@@ -45,11 +47,21 @@ public class Day10 extends AbstractDay {
         return total;
     }
 
-    public int multiplyGF2(int[] cols, int vec) {
-        int result = 0;
-        for (int i = 0; i < cols.length; i++) {
-            if ((vec & (1 << i)) != 0)
-                result ^= cols[i];
+    @Override
+    public Integer part2() {
+        for (Machine m : machines) {
+            var sol = solve(m.buttons, m.joltages, 1e-9);
+            System.out.println("particular: " + Arrays.toString(sol.left()));
+            System.out.println("nullspace: " + Arrays.deepToString(sol.right()));
+        }
+        return null;
+    }
+
+    public int getSol(IntObjectPair<int[]> sol, int x) {
+        int result = sol.leftInt();
+        for (int i = 0; i < sol.right().length; i++) {
+            if ((x & (1 << i)) != 0)
+                result ^= sol.right()[i];
         }
         return result;
     }
@@ -63,7 +75,7 @@ public class Day10 extends AbstractDay {
                 lhs[y] |= (1 << x);
 
         // Row reduce
-        int[] pivotCols = new int[diagram.length];
+        int[] pivotCols = new int[Math.min(diagram.length, buttons.length)];
         int[] freeCols = new int[buttons.length];
         int rank = 0;
         int nullity = 0;
@@ -110,6 +122,70 @@ public class Day10 extends AbstractDay {
             nullspace[nx] |= (1 << freeCols[nx]);
 
         return IntObjectPair.of(particular, nullspace);
+    }
+
+       private Pair<double[], double[][]> solve(int[][] buttons, int[] joltages, double epsilon) {
+        // Build augmented matrix, rows are bit vectors
+        double[][] lhs = new double[joltages.length][buttons.length];
+        for (int x = 0; x < buttons.length; x++)
+            for (int y : buttons[x])
+                lhs[y][x] = 1.0;
+        double[] rhs = new double[joltages.length];
+        for (int y = 0; y < joltages.length; y++)
+            rhs[y] = joltages[y];
+
+        // Row reduce
+        int[] pivotCols = new int[Math.min(joltages.length, buttons.length)];
+        int[] freeCols = new int[buttons.length];
+        int rank = 0;
+        int nullity = 0;
+        for (int x = 0; x < buttons.length; x++) {
+            boolean foundPivot = false;
+            for (int y = rank; y < joltages.length; y++) {
+                if (Math.abs(lhs[y][x]) > epsilon) {
+                    double[] lhsTemp = lhs[rank]; lhs[rank] = lhs[y]; lhs[y] = lhsTemp;
+                    double rhsTemp = rhs[rank]; rhs[rank] = rhs[y]; rhs[y] = rhsTemp;
+                    foundPivot = true;
+                    break;
+                }
+            }
+            if (!foundPivot) {
+                freeCols[nullity++] = x;
+            } else {
+                double factor = lhs[rank][x];
+                for (int i = x; i < buttons.length; i++)
+                    lhs[rank][i] /= factor;
+                rhs[rank] /= factor;
+
+                for (int y = 0; y < joltages.length; y++) {
+                    if (y == rank) continue;
+                    if (Math.abs(lhs[y][x]) > epsilon) {
+                        for (int i = x; i < buttons.length; i++)
+                            lhs[y][i] -= lhs[rank][i];
+                        rhs[y] -= rhs[rank];
+                    }
+                }
+                pivotCols[rank++] = x;
+            }
+        }
+
+        // Check valid solution
+        for (int y = rank; y < joltages.length; y++)
+            if (DoubleStream.of(lhs[y]).allMatch(x -> Math.abs(x) <= epsilon) && Math.abs(rhs[y]) > epsilon)
+                throw new IllegalArgumentException("no solution");
+
+        // Build solution space
+        double[] particular = new double[buttons.length];
+        double[][] nullspace = new double[nullity][buttons.length];
+        for (int y = 0; y < rank; y++) {
+            particular[pivotCols[y]] = rhs[y];
+            for (int nx = 0; nx < nullity; nx++)
+                nullspace[nx][pivotCols[y]] = Math.abs(lhs[y][freeCols[nx]]);
+        }
+        for (int nx = 0; nx < nullity; nx++)
+            nullspace[nx][freeCols[nx]] = 1;
+
+        return Pair.of(particular, nullspace);
     }
 
     public record Machine(int[] diagram, int[][] buttons, int[] joltages) {
